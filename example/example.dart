4dece49cbdcb260bcca2b58a6149c71da5d21b53
Moonspeaker <54249358+Moonspeaker@users.noreply.github.com>
2019-11-05 20:51:33 +0300

    Initial commit

---
+++ example/example.dart
@@ -0,0 +1,3 @@
+import 'package:bitmask32/bitmask32.dart';
+
+void main() => print(encodeBitMask([0,1, 31, 2]));
---
+++ lib/bitmask32.dart
@@ -0,0 +1,22 @@
+library bitmask32;
+
+const int _oneBit = 0x01;
+
+///Takes a list of integers 31 .. 0, representing indexes of 1
+int encodeBitMask(final List<int> positions){
+  int value = 0;
+  positions.forEach((final int position) => value |= (_oneBit << position));
+  return value;
+}
+
+List<int> decodeBitMask(int bitMask){
+  final List<int> bits = List();
+  for(int i = 31; i >= 0; i--){
+    final int power = _oneBit << i;
+    if(bitMask >= power){
+      bitMask -= power;
+      bits.add(i);
+    }
+  }
+  return bits;
+}
